fab0c81a2bb6ad24383e43775c80ab2f6f47166e
Ayush <nairayush45@gmail.com>
2025-03-06 20:28:35 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { Routes, Route} from 'react-router-dom'
+import Home from '../pages/Home.jsx'
+import CreateBook from '../pages/CreateBook.jsx'
+import EditBook from '../pages/EditBook.jsx'
+import DeleteBook from '../pages/DeleteBook.jsx'
+import ShowBook from '../pages/ShowBook.jsx'
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/books/create' element={<CreateBook/>}/>
+      <Route path='/books/edit/:id' element={<EditBook/>}/>
+      <Route path='/books/delete/:id' element={<DeleteBook/>}/>
+      <Route path='/books/show/:id' element={<ShowBook/>}/>
+    </Routes>
   )
 }
 
